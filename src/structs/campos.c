c47f309a5e3b99cc97fbc94b95edba3ab2eaa2d0
Arthur Queiroz <arthurquimu@gmail.com>
2023-05-14 20:46:03 -0300

    Comment campos

--- src/structs/campos.c
+++ src/structs/campos.c
@@ -1,6 +1,14 @@
 #include "campos.h"
 #include "../extras/utils.h"
 
+/**
+ * le da entrada padrão um campo inteiro
+ * 
+ * @param inteiro ponteiro para o inteiro
+ * 
+ * se NULO, bota -1
+ * se não, lê normalmente
+*/
 void le_campo_inteiro(int* inteiro) {
     char c = getc(stdin);
     if (( c = getc(stdin)) == 'N') {
@@ -14,6 +22,11 @@ void le_campo_inteiro(int* inteiro) {
     }
 }
 
+/**
+ * le da entrada padrão um campo de busca
+ * 
+ * @return ponteiro para o campo de busca
+*/
 campo_busca_t* le_campo_busca() {
     campo_busca_t* campo_atual = (campo_busca_t*) malloc(sizeof(campo_busca_t));
     if (campo_atual == NULL) return NULL;
@@ -28,6 +41,12 @@ campo_busca_t* le_campo_busca() {
     return campo_atual;
 }
 
+/**
+ * lê o numero especificado de campos de busca e aloca eles
+ * 
+ * @param num_campos numero a ser lido
+ * @return vetor (ponteiro) de ponteiro de campos
+*/
 campo_busca_t** le_campos_busca(int num_campos) {
 
     campo_busca_t** campos_buscados = (campo_busca_t**) malloc(num_campos * sizeof(campo_busca_t*));
@@ -44,12 +63,20 @@ campo_busca_t** le_campos_busca(int num_campos) {
     return campos_buscados;
 }
 
+/**
+ * retorna se registro tem campo com valor especificado
+ * 
+ * @param crime_atual registro atual
+ * @param campo_atual campo atual (contem nome do campo e valor)
+ * @return 1 se registro tem, 0 se não
+*/
 int satisfaz_campo(crime_t* crime_atual, campo_busca_t* campo_atual) {
     if (strcmp(campo_atual->campo_busca, "idCrime") == 0) {
         return crime_atual->idCrime == campo_atual->chaveBuscaInt;
     } else if (strcmp(campo_atual->campo_busca, "numeroArtigo") == 0) {
         return crime_atual->numeroArtigo == campo_atual->chaveBuscaInt;
     } else if (strcmp(campo_atual->campo_busca, "dataCrime") == 0) {
+        // uso a flag dinamica 1 para indicar que campo_atual->chaveBuscaStr termina com \0
         return compara_string_limitada(crime_atual->dataCrime, campo_atual->chaveBuscaStr, 10, 1) == 0;
     } else if (strcmp(campo_atual->campo_busca, "marcaCelular") == 0) {
         return compara_string_limitada(crime_atual->marcaCelular, campo_atual->chaveBuscaStr, 12, 1) == 0;
@@ -64,9 +91,17 @@ int satisfaz_campo(crime_t* crime_atual, campo_busca_t* campo_atual) {
 
         return strcmp(campo_atual->chaveBuscaStr, string_dinamica_original) == 0;
     }
-    return -1;
+    return -1; // inutil, nunca chega aqui
 }
 
+/**
+ * retorna se registro satisfaz vários campos
+ * 
+ * @param crime_atual registro atual
+ * @param query_atual vetor de campos
+ * @param n_campos numero de campos desse vetor
+ * @return 1 se satisfaz a query, 0 se nao
+*/
 int satisfaz_query(crime_t* crime_atual, campo_busca_t** query_atual, int n_campos) {
     int resposta = 1; // satisfaz. Se eu encontrar evidencias do contrario, seto pra zero
 
@@ -83,7 +118,13 @@ int satisfaz_query(crime_t* crime_atual, campo_busca_t** query_atual, int n_camp
     return resposta;
 }
 
-
+/**
+ * atualiza campo de registro
+ * 
+ * @param crime_atual registro atual
+ * @param campo_atual campo (atualizar com base nele)
+ * @return 1 se houve sucesso, 0 se nao (erro de alocacao)
+*/
 int atualiza_campo(crime_t* crime_atual, campo_busca_t* campo_atual) {
     if (strcmp(campo_atual->campo_busca, "idCrime") == 0) {
         crime_atual->idCrime = campo_atual->chaveBuscaInt;
@@ -115,9 +156,17 @@ int atualiza_campo(crime_t* crime_atual, campo_busca_t* campo_atual) {
 
         return 1;
     }
-    return -1;
+    return -1; //nunca chega aqui
 }
 
+/**
+ * atualiza registro com base em multiplas atualizações
+ * 
+ * @param crime_atual registro atual
+ * @param atualizacoes vetor de campos, cada campo corresponde a uma atualizacao
+ * @param n_campos tamanho do vetor
+ * @return 1 se houve sucesso, 0 se nao (erro)
+*/
 int atualiza_registro(crime_t* crime_atual, campo_busca_t** atualizacoes, int n_campos) {
     campo_busca_t* campo_atual;
 
@@ -129,6 +178,14 @@ int atualiza_registro(crime_t* crime_atual, campo_busca_t** atualizacoes, int n_
     return 1;
 }
 
+/**
+ * checa se no vetor de campos "campos" ha algum campo de nome "nome_campo"
+ * 
+ * @param campos vetor de campos
+ * @param n_campos tamanho do vetor
+ * @param nome_campo nome de campo
+ * @return numero i >= 0 indicando que campos[i] tem esse nome ou -1
+*/
 int checa_campo_procurado(campo_busca_t** campos, int n_campos, char* nome_campo) {
     for (int i = 0; i < n_campos; i++) {
         if (strcmp(campos[i]->campo_busca, nome_campo) == 0) {
--- src/structs/campos.h
+++ src/structs/campos.h
@@ -5,12 +5,16 @@
     #include "utils_structs.h"
     #include <stdio.h>
 
+    // mais detalhes no .c
 
+    // define registro de campo de busca
+    // util para quando usuario esta especificando esses campos de busca
+    // na entrada
     typedef struct Campo_busca campo_busca_t;
     struct Campo_busca {
-        char campo_busca[20];
-        int chaveBuscaInt;
-        char chaveBuscaStr[100];
+        char campo_busca[20]; // por ex "idCrime" ou "lugarCrime" (etc)
+        int chaveBuscaInt; // se for um campo de int como "idCrime", armazena seu valor
+        char chaveBuscaStr[100]; // se for campo de string, analogamente
     };
 
     campo_busca_t** le_campos_busca(int num_campos);
